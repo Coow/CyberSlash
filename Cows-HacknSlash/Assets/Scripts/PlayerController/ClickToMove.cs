5794cf403158bbceae2adb20afc2e87858d4cc4c
Coow <eirik_skarding@hotmail.no>
2018-08-25 14:27:38 +0200

    Basic Clicking Movement

---
+++ Cows-HacknSlash/Assets/Scripts/PlayerController/ClickToMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour {
+
+	private NavMeshAgent mNavMeshAgent;
+	private bool mRunning;
+	// Use this for initialization
+	void Start () {
+		mNavMeshAgent = GetComponent<NavMeshAgent>();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		RaycastHit hit;
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			if(Physics.Raycast(ray, out hit, 100))
+			{
+				mNavMeshAgent.destination = hit.point;
+			}
+		}
+
+		//When animations get added
+		if(mNavMeshAgent.remainingDistance <= mNavMeshAgent.stoppingDistance)
+		{
+			mRunning = false;
+		} else {
+			mRunning = true;
+		}
+
+	}
+}
