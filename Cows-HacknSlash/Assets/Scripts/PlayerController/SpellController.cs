277c058a07f6d5038c8b2a9f7a3b29cd9081c777
MissingTech <biscuitgamer31@gmail.com>
2018-09-09 12:44:20 +0100

    Fireballs don't phase through floor
    Fireballs stay at a constant height - will have them destroy when collide or stay at certain height above ground.

--- Cows-HacknSlash/Assets/Scripts/PlayerController/SpellController.cs
+++ Cows-HacknSlash/Assets/Scripts/PlayerController/SpellController.cs
@@ -9,11 +9,18 @@ public class SpellController : MonoBehaviour {
 	[Header("FireBall")]
 	public GameObject fireball;
     public float FireBallSpeed;
+	private float distance = 10f;
 
 	void Update () {
 		
 		if(Input.GetKeyDown(KeyCode.E)){	
-			Vector3 aimPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+     
+        	var position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        	position = Camera.main.ScreenToWorldPoint(position);
+        	var spell = Instantiate(fireball, transform.position, Quaternion.identity) as GameObject;
+        	spell.transform.LookAt(position);    
+        	//spell.GetComponent<Rigidbody>().AddForce(spell.transform.forward * 3000);
+			/*Vector3 aimPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			aimPos.z = 0;
 
 			var projectile = Instantiate(fireball, transform.position,Quaternion.identity);
@@ -21,7 +28,7 @@ public class SpellController : MonoBehaviour {
 			Debug.Log("Aimpos = " + aimPos);
 
 			//projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * (FireBallSpeed + 2));
-			//Debug.Log("Speed =" + projectile.transform.forward * (fireBallSpeed + 2));		
+			//Debug.Log("Speed =" + projectile.transform.forward * (fireBallSpeed + 2));*/		
 		}
 		/*if(Input.GetKeyDown(KeyCode.E)){
 
--- Cows-HacknSlash/Assets/Scripts/Spells/FireBallBehaviour.cs
+++ Cows-HacknSlash/Assets/Scripts/Spells/FireBallBehaviour.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireBallBehaviour : MonoBehaviour {
-    public float Speed = 2f;
+    public float speed = 2f;
 
     private Rigidbody _rigidbody;
 	
 	void Start() {
 		_rigidbody = gameObject.GetComponent<Rigidbody>();
+        _rigidbody.AddForce(this.transform.forward * speed);
 	}
 	void FixedUpdate() {
         //_rigidbody.AddForce(transform.forward * (Speed + 2));
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        _rigidbody.AddForce(ray.direction * Speed, ForceMode.Impulse);
+        //var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        _rigidbody.MovePosition(new Vector3(this.transform.position.x, 2f, this.transform.position.z));
 	}
 }
