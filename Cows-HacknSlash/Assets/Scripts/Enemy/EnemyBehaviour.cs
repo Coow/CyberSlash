aca3a7811a962cea3b4138a1ba64bbfdc326bbb2
MadCoderr <msrfarooqi@gmail.com>
2018-09-10 14:13:05 +0500

    Enemy Behaviour Update
    Now Enemy will follow player when player come across a certain distance
    and enemy would attack player when enemy reach to player at certain distance

--- Cows-HacknSlash/Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Cows-HacknSlash/Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -8,13 +8,56 @@ public class EnemyBehaviour : MonoBehaviour {
     [SerializeField]
     private Transform Target;
 
+    [SerializeField]
+    private Transform SwordArmPivot;
+
+    [SerializeField]
+    private float ActivateRange;
+
     private NavMeshAgent _agent;
+    private bool _startMoving = false;
 
     private void Start() {
         _agent = GetComponent<NavMeshAgent>();
     }
 
     void Update () {
-        _agent.SetDestination(Target.position);
+        var distance = Vector3.Distance(transform.position, Target.position);
+
+        // If player reached a certain distance close to enemy. Enemy would start following player.
+        if (distance <= ActivateRange) {
+            _startMoving = true;
+        }
+
+        if (_startMoving) {
+            _agent.SetDestination(Target.position);
+        }
+
+        // If enemy reached to stopping distance. Let the enemy start swing the sword.
+        if (ReachedToPlayer()) {
+            RotateTowardsPlayer();
+            SwordArmPivot.rotation = Quaternion.Lerp(Quaternion.Euler(0, 0, 20), Quaternion.Euler(0, 0, -20), Mathf.PingPong(2 * Time.time, 1f));
+        }
+
 	}
+
+    private bool ReachedToPlayer() {
+        if (!_agent.pathPending) {
+            if (_agent.remainingDistance != 0 && _agent.remainingDistance <= _agent.stoppingDistance) {
+                if (!_agent.hasPath || _agent.velocity.sqrMagnitude == 0f) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void RotateTowardsPlayer() {
+        Vector3 directon = (Target.position - transform.position).normalized;
+        directon.y = 0;
+        Quaternion lookRotation = Quaternion.LookRotation(directon);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        SwordArmPivot.rotation = Quaternion.LookRotation(directon);
+    }
 }
