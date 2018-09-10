f4b36ebd7be00b53361b248711c4d1cb72063d0b
MadCoderr <msrfarooqi@gmail.com>
2018-09-10 05:43:04 +0500

    Basic Enemy AI

---
+++ Cows-HacknSlash/Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private Transform Target;
+
+    private NavMeshAgent _agent;
+
+    private void Start() {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update () {
+        _agent.SetDestination(Target.position);
+	}
+}
