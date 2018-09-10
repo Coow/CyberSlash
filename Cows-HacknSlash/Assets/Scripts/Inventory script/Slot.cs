d1c761f2183936d8ab004aef3ceb875825f08ffa
Albin Haglund <haggespel@gmail.com>
2018-09-10 20:17:31 +0100

    Inventory 0.02 by Hagge

---
+++ Cows-HacknSlash/Assets/Scripts/Inventory script/Slot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Slot : MonoBehaviour {
+    //if something is in it
+    public bool somethingin;
+
+    //if the stack is full
+    public bool full;
+
+    //what item the slot is holding
+    public string iteminit;
+}
