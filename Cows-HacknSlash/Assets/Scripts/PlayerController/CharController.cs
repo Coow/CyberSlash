2764a1b76188851a822249d3d29aed4c8c169dcf
Coow <eirik_skarding@hotmail.no>
2018-10-18 18:42:58 +0200

    Fixed issue with NavMeshAgent

--- Cows-HacknSlash/Assets/Scripts/PlayerController/CharController.cs
+++ Cows-HacknSlash/Assets/Scripts/PlayerController/CharController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class CharController : MonoBehaviour {
 
 	private NavMeshAgent navMeshAgent;
 	private bool running;
 	[SerializeField]
 	private LayerMask layerMask;
+	public float KnockoutTime = 1.5f;
+	private bool _knockedOut = false;
 	
 	[Header("Misc")]
 	public GameObject cursorClick;
@@ -26,9 +29,10 @@ public class CharController : MonoBehaviour {
 		Vector3 pointToLook = ray.GetPoint(50f);
 		Debug.DrawLine(ray.origin, pointToLook, Color.blue);
 		
-		if (Input.GetMouseButton(0)) {
+		if (Input.GetMouseButton(0) & ) {
 			if (Physics.Raycast(ray, out hit, 100, layerMask)) {
 				navMeshAgent.destination = hit.point;
+				navMeshAgent.updatePosition = true;
 			}
 		}
 
@@ -37,12 +41,14 @@ public class CharController : MonoBehaviour {
 				Vector3 spawnPoint = hit.point + cursorOffset;
 
 				Instantiate(cursorClick, spawnPoint ,Quaternion.identity);
+				navMeshAgent.updatePosition = true;	
 			}
 		}
 
 		// When animations get added.
 		if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance) {
 			running = false;
+			navMeshAgent.updatePosition = false;
 		} else {
 			running = true;
 		}
@@ -61,4 +67,8 @@ public class CharController : MonoBehaviour {
     private void DestroyPlayer() {
         Destroy(this.gameObject);
     }
+
+	public void KnockedOut(){
+
+	}
 }
