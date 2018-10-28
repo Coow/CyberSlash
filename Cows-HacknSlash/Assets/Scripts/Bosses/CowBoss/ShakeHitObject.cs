52f755e748e9b3284615f5fab5f75c06969d7753
Coow <eirik_skarding@hotmail.no>
2018-10-28 22:45:38 +0100

    Hopefully fixed issue and added Walking Anim for player

--- Cows-HacknSlash/Assets/Scripts/Bosses/CowBoss/ShakeHitObject.cs
+++ Cows-HacknSlash/Assets/Scripts/Bosses/CowBoss/ShakeHitObject.cs
@@ -3,14 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShakeHitObject : MonoBehaviour {
-
-	private Animator animator;
-
-	void Start() {
-		animator = GetComponent<Animator>();
-	}
-	// Yes.. We need to use Awake here
-	void Awake () {
-		animator.SetTrigger("SHAKE");
-	}
+    
 }
