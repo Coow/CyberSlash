13c61a6038136c93f7d62454c72412f0fcb10b75
Coow <eirik_skarding@hotmail.no>
2019-04-06 18:25:41 +0200

    Added Hierachy Icons!
    A good old favorite

--- Cows-HacknSlash/Assets/Scripts/Enemy/FlyingDragon.cs
+++ Cows-HacknSlash/Assets/Scripts/Enemy/FlyingDragon.cs
@@ -16,21 +16,41 @@ public class FlyingDragon : MonoBehaviour {
 	[Range(0.0f, 5f)]
 	private float animationSpeed = 2.5f;
 
-	// Use this for initialization
+	[Header("Sound Effects")]
+	public AudioClip aggro;
+	public AudioClip damage;
+	public AudioClip dying;
+	public AudioClip walk;
+
+	private AudioSource source;
+    private float lowPitchRange = .75F;
+    private float highPitchRange = 1.5F;
+    private float velToVol = .2F;
+    private float velocityClipSplit = 10F;
+
+
 	void Start () {
 		anim_Controller = GetComponent<Animator>();
 		navMeshAgent = GetComponent<NavMeshAgent>();
 		anim_Controller.SetBool("running", true);
+		
+		source = GetComponent<AudioSource>();
 	}
 	
 	void FixedUpdate(){
 
 		anim_Controller.speed = animationSpeed;
 
-		if(Vector3.Distance(Target.transform.position, transform.position) <= Range)
-    	{
-    		navMeshAgent.destination = Target.transform.position;
-    	}
+		
+
+		if(Target.gameObject != null) {
+			if(Vector3.Distance(Target.transform.position, transform.position) <= Range)
+			{
+				navMeshAgent.destination = Target.transform.position;
+			}
+		} else {
+			Target = GameObject.Find("char");
+		}
 
 		
 		if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance) {
@@ -41,5 +61,11 @@ public class FlyingDragon : MonoBehaviour {
 			m_Running = true;
 			anim_Controller.SetBool("running", m_Running);
 		}
+	}
+
+	void PlayWalkSound(){
+        source.pitch = Random.Range (lowPitchRange,highPitchRange);
+
+		source.PlayOneShot(damage,0.5f);
 	}
 }
