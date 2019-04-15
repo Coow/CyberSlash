82915d9ff3af26b016a2ae677d37a5af3a3d0ef7
Coow <eirik_skarding@hotmail.no>
2019-04-15 14:59:07 +0200

    Added ability to ToggleSkybox, needs GUI tho

---
+++ Cows-HacknSlash/Assets/Scripts/PlayerController/Settings/SkyboxSetting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[RequireComponent(typeof(Skybox))]
+public class SkyboxSetting : MonoBehaviour {
+
+	public bool skyboxEnabled = true;
+	private Camera camera;
+	private Skybox skybox;
+
+	void Start() {
+		camera = GetComponent<Camera>();
+		skybox = GetComponent<Skybox>();
+	}
+
+	void FixedUpdate() {
+		if(skyboxEnabled) {
+			skybox.enabled = true;
+			camera.clearFlags = CameraClearFlags.Skybox;
+			Debug.Log("Skybox is enabled");
+		}
+
+		if(!skyboxEnabled) {
+			camera.clearFlags = CameraClearFlags.SolidColor;
+			skybox.enabled = false;
+			Debug.Log("Skybox disabled");
+		}
+	}
+}
