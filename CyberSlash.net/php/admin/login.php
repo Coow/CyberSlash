103b6f0d664e7e387e76eeedbfd20139c1840300
Coow <eirik_skarding@hotmail.no>
2019-09-30 18:56:23 +0200

    Removing the Website, Discord bot and Game Builds folder
    Moving these to seperate repos..
    Since there is no pointin having the Discord Bot and Website for people who wants to work on the game, and the other way around

--- CyberSlash.net/php/admin/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-$loginURL = $_SERVER['HTTP_HOST'].'/php/userpages/loginpage.php';
-header('location: https://'.$loginURL);
-?>
-
--- CyberSlash.net/php/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-//include config
-include $_SERVER['DOCUMENT_ROOT'].'/includes/config.php';
-
-//log user out
-$user->logout();
-
-?>
